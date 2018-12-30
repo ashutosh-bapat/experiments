8ef57a585f5fb5b1e91fc8f6b29e0cd2d923eef5
Ashutosh Bapat <ashutosh.bapat.oss@gmail.com>
2018-12-30 18:37:24 +0530

    Improve EventTimeAggregator test
    Thread.sleep() is not accurate and thus when using it to check accuracy of EventTimeAggregator
    allow a variance of 1% in the measurement.
    
    Ashutosh Bapat

--- src/test/java/com/ashutosh/utils/EventTimeAggregatorTest.java
+++ src/test/java/com/ashutosh/utils/EventTimeAggregatorTest.java
@@ -8,14 +8,15 @@ public class EventTimeAggregatorTest {
 
     @Test
     public void testUsingSleep() throws InterruptedException {
-        testWithParams(5, 3, 20);
-        testWithParams(1, 3, 20);
+        testWithParams(5, 3, 100);
+        testWithParams(1, 3, 100);
     }
 
     private void testWithParams(int period, int numPeriods, int sleepMS) throws InterruptedException {
         int i;
         EventTimeAggregator eta = new EventTimeAggregator("sleep tester", period);
-        int totalExpectedTime = 0;
+        float totalExpectedTime = 0;
+        float permissibleErrorPct = 5;
 
         for (i = 1; i <= period * numPeriods; i++) {
             eta.eventStart();
@@ -23,12 +24,19 @@ public class EventTimeAggregatorTest {
             eta.eventEnd();
 
             if (i % period == 0) {
-                assertEquals(sleepMS, (int) eta.getPeriodicAverage());
+                verifyExpectedValueWithError(eta.getPeriodicAverage(), sleepMS, permissibleErrorPct);
+                totalExpectedTime = totalExpectedTime + eta.getPeriodicAverage() * period;
             }
-            totalExpectedTime = totalExpectedTime + sleepMS;
         }
-        assertEquals(sleepMS, (int) eta.getTotalAverage());
-        assertTrue("Error exceeded 1 millisecond accuracy.",
-                totalExpectedTime <= (int) eta.getTotalTime() && totalExpectedTime + 1 >= eta.getTotalTime());
+        verifyExpectedValueWithError(eta.getTotalAverage(), sleepMS, permissibleErrorPct);
+        verifyExpectedValueWithError(eta.getTotalTime(), totalExpectedTime, permissibleErrorPct);
+    }
+
+    private void verifyExpectedValueWithError(float actualAvg, float expectedAvg, float errorPct) {
+        float error = expectedAvg * errorPct / 100;
+        assertTrue("Actual value " + actualAvg + " less than expected value " + expectedAvg + " by more than permissible error " + error,
+                actualAvg >= expectedAvg - error);
+        assertTrue("Actual value " + actualAvg + " more than expected value " + expectedAvg + " by more than permissible error " + error,
+                actualAvg <= expectedAvg + error);
     }
 }
